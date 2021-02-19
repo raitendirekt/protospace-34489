0e57bfce0a579dd49530b2329e485df0c1f8e94a
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-02-19 14:18:02 +0900

    edit prototypes_controller's edit action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -23,6 +22,9 @@ class PrototypesController < ApplicationController
   end
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
   def update
     @prototype = Prototype.find(params[:id])
@@ -41,11 +43,6 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-  def move_to_index
-    unless current_user == @prototype 
-      redirect_to action: :index
-    end
   end
 
 end
